d4e0e37c9468c278b9db9714d7a6fd418f9fc20f
Vincent <wendong@ebay.com>
2018-08-08 17:04:15 -0700

    add connect-state example

---
+++ connecting-state/src/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { combineReducers, createStore } from 'redux';
+import './index.css';
+
+// action types
+const TODO_ADD = 'TODO_ADD';
+const TODO_TOGGLE = 'TODO_TOGGLE';
+const FILTER_SET = 'FILTER_SET';
+
+// reducers
+const todos = [
+  { id: '0', name: 'learn redux' },
+  { id: '1', name: 'learn mobx' },
+];
+
+function todoReducer(state = todos, action) {
+  switch (action.type) {
+    case TODO_ADD: {
+      return applyAddTodo(state, action);
+    }
+    case TODO_TOGGLE: {
+      return applyToggleTodo(state, action);
+    }
+    default: return state;
+  }
+}
+
+function applyAddTodo(state, action) {
+  const todo = Object.assign({}, action.todo, { completed: false });
+  return state.concat(todo);
+}
+
+function applyToggleTodo(state, action) {
+  return state.map(todo =>
+    todo.id === action.todo.id
+      ? Object.assign({}, todo, { completed: !todo.completed })
+      : todo
+  );
+}
+
+function filterReducer(state = 'SHOW_ALL', action) {
+  switch (action.type) {
+    case FILTER_SET: {
+      return applySetFilter(state, action);
+    }
+    default: return state;
+  }
+}
+
+function applySetFilter(state, action) {
+  return action.filter;
+}
+
+// action creators
+function doAddTodo(id, name) {
+  return {
+    type: TODO_ADD,
+    todo: { id, name },
+  };
+}
+
+function doToggleTodo(id) {
+  return {
+    type: TODO_TOGGLE,
+    todo: { id },
+  };
+}
+
+function doSetFilter(filter) {
+  return {
+    type: FILTER_SET,
+    filter,
+  };
+}
+
+// store
+const rootReducer = combineReducers({
+  todoState: todoReducer,
+  filterState: filterReducer,
+});
+
+const store = createStore(rootReducer);
+
+// View layer
+function TodoApp() {
+  return <div>Todo App</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
